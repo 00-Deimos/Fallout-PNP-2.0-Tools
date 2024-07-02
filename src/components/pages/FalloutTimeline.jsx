0d8cca18d7d6ec620dfa58f45562efdc0a6c0548
Santiago <jmsg.santiago@protonmail.com>
2024-07-02 23:48:38 +0100

    Criado o conteudo para a pagina da timeline.

--- src/components/pages/FalloutTimeline.jsx
+++ src/components/pages/FalloutTimeline.jsx
@@ -1,6 +1,42 @@
 export default function FalloutTimeline () {
 
     return (
-        <div>Fallout Timeline</div>
+        <main>
+            <h2>The timeline of Fallout</h2>
+            <blockquote>
+                <p>"In <time dateTime="2077">2077</time>, the storm of world war had come again. In two brief hours, most of the planet was reduced to cinders. And from the ashes of nuclear devastation, a new civilization would struggle to arise."</p>
+                <footer>— The Narrator, Fallout Intro</footer>
+                <audio controls>
+                    <source src="/audio/Fo1_Narrator_StormOfWorldWar.ogg" type="audio/ogg" /> 
+                </audio>  
+            </blockquote>
+            <p>The franchise's timeline stretches from pre-War years, to the "future of the fifties," to the decades following the destruction of the earth by nuclear war. With the goal of exploring ideas revolving around a futuristic, post-nuclear world, the game setting exists on a timeline that branched after World War II. This fork led to a world where technology progressed quickly while maintaining the cultural norms of the mid-century. A worldwide shortage of resources such as petroleum and uranium led to a series of conflicts between the United States, Canada, China, and European powers that were known as the Resource Wars. These conflicts culminated in the breakdown of the United Nations and deployment of nuclear missiles at and by the United States, a global nuclear event known as the Great War. These events culminated in the breakdown of the United Nations and deployment of nuclear missiles at and by the United States.Prior to this, some citizens had elected to purchase space in underground vaults, most of which shielded those within from the nuclear event. Those who survived outside of the vaults or who emerged from underground created new communities, rebuilding in what remained of the country</p>
+            <ul>
+                <li>Contents
+                    <ul>
+                        <li><a href="#resWars">Resource Wars</a></li>
+                        <li><a href="#greatWar">Great War</a></li>
+                        <li><a href="#newChapter">A New Chapter on Human History</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <section id="resWars">
+                <h3>Resource Wars</h3>
+                <p>Several violent engagements took place, including the annexation of Canada by the United States and the Chinese invasion of Anchorage, Alaska. European powers fought in the Middle East. Once the Chinese were pushed from Alaska, United States forces were deployed to mainland China, where they experienced a stalemate. The United States closed its borders on <time dateTime="2052-03-05">March 5, 2052</time>.</p>
+                <p>In the United States, anti-Communist propaganda was created which fueled paranoia. The United States Army also commissioned the development of Liberty Prime, intended to be a superweapon. Troops such as the 5th Infantry were stationed overseas on the Island of Mambajao. The nuclear threat was thought contained at the time, as an ideal nuclear first strike scenario (according to DIA intelligence at the time) would only succeed in destroying 41% of the American nuclear stockpile. At Fort Strong, T-51 Power Armor was developed and subsequently shipped to China, where they were reported as effective in gaining troop surrenders. Troops that had already completed their tours of duty were redeployed</p>
+            </section>
+            <section id="greatWar">
+                <h3>Great War</h3>
+                <p>The event ended decade-long conflict between the United States and China. By <time dateTime="2077">2077</time>, a combination of attrition, loss of supply routes, and offensives conducted by the United States across the front left Chinese units in Alaska stranded and cut off from reinforcements. The liberation of Anchorage aided by winterized T-51 Power Armor units under the command of General Constantine Chase marked the end of the Anchorage Reclamation. After United States victory in Anchorage, the conflict transitioned into a stalemate. Media outlets reported billions of dollars wasted with no clear end or victor in sight Preparing for a nuclear or biological attack, the president of the United States retreated to an oil rig off the west coast in <time dateTime="2077-03">March 2077</time>.</p>
+                <p>The war both began and ended on <time dateTime="2077-10-23">October 23, 2077</time>. Submarines were identified by COMPACFLT at  <time dateTime="2077-10-23T00:03-05:00">00:03 EST</time> off the West Coast. At <time dateTime="2077-10-23T03:37-05:00">03:37 EST</time>, a squadron of high-altitude bombers were sighted off the Bering Strait. Six hours later, at <time dateTime="2077-10-23T09:13-05:00">09:13 EST</time>, IONDS detected the first four missile launches and the United States moved to DEFCON 2.</p>
+                <p>Four minutes later, NORAD reported confirmed strikes. Los Angeles was struck several times in the city center. Citizens of Washington received warnings after the West Coast was already destroyed. Unclear where the bombs came from or who initiated launch first, air raid sirens sounded, and at <time dateTime="2077-10-23T09:26-05:00">09:26 EST</time>, the president ordered a retaliatory strike according to scenario MX-CN91. Nuclear bombs hit Pennsylvania and New York at <time dateTime="2077-10-23T09:42-05:00">09:42 EST</time>. By <time dateTime="2077-10-23T09:47-05:00">09:47 EST</time>, facilities had gone offline.</p>
+                <p>The nuclear exchange continued for two hours. The Chinese stealth submarine Yangtze launched its missiles at the American east coast from the Boston waterway. Las Vegas was spared a direct nuclear strike as Mr. House had also prepared for the inevitability, disabling or destroying 68 out of the 77 warheads heading for the city. A relative few were able to reach safety in underground vaults constructed by the Vault-Tec Corporation.</p>
+                <p>Although emergency response units and elements of the National Guard were mobilized on the evening of <time dateTime="2077-10-22">October 22</time>, their ability to provide aid to victims and refugees was decreased by the lack of any centralized command and control, resulting from electromagnetic pulses which disabled communications equipment. Despite efforts by the military to bolster relief efforts, disarray and the onset of radiation sickness resulted in many deserting their posts. Other first responders persisted, mobilizing to provide order and crisis relief. The resulting aftermath of the war led to the deaths of 7.54 billion people.</p>
+            </section>
+            <section id="newChapter">
+                <h3>A New Chapter on Human History</h3>
+                <p>Despite the world being plunged into an abyss of nuclear fire and radiation, it was not, as some had predicted, the end of the world. Instead, the apocalypse was simply the prologue to another chapter of human history. When atomic fire consumed the earth, those who survived did so in great, underground vaults. When they opened, their inhabitants set out across ruins of the old world to build new societies, establish new villages, forming tribes. The future of the world is uncertain, and it's up to the players to decide how they will you re-shape the world. Welcome to the Wasteland. Welcome to the world of Fallout</p>
+            </section>
+        </main>
     );
 }
