46dab7c5c2c8ebf38b8db16c7eff925806067caa
Santiago <jmsg.santiago@protonmail.com>
2024-07-03 02:04:02 +0100

    Criado o conteudo para a pagina de golossario e termos.

--- src/components/pages/Glossary.jsx
+++ src/components/pages/Glossary.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 export default function Glossary () {
 
+    const [terms, setTerms] = useState([]);
+
+    useEffect (() => {
+        fetch("/data/pages_data/glossaryTerms.json")
+            .then(response => response.json () )
+            .then( terms => {
+                setTerms(terms);
+            });
+    }, []);
+
     return (
-        <div>Glossary</div>
+        <main>
+            <h2>Glossary of Terms and Abbreviations in the Fallout Universe</h2>
+            <p>The following table will present you with all the terms and abbreviations that you need to know to better understand the game.</p>
+            <h3>Terms and Abbreviations</h3>
+            <dl>
+                {terms.map(term => (
+                    <span key={term.id}>
+                        <dt>
+                            {term.link !== '' ? (
+                                <NavLink to={term.link}>{term.name}</NavLink>
+                            ) :
+                            (
+                                <span>{term.name}</span>
+                            )}  
+                        </dt>
+                        <dd>{term.definition}</dd>  
+                    </span>
+                ))}        
+            </dl>
+        </main>
     );
 }
